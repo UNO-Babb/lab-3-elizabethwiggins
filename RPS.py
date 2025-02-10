24dada8e04af9dd15c17456a25473f2426faca33
elizabethwiggins <ewiggins@unomaha.edu>
2025-02-10 02:20:57 +0000

    sesh 2

--- RPS.py
+++ RPS.py
@@ -12,9 +12,13 @@ def main():
   #User can play as many games as they wish.
 
   #Randomly choose the computer between 'R', 'P', or 'S'
+  import random
   #Prompt the user for their RPS selection
+  input("Select Rock Paper or Scissors (R, P, or S):")
+  response=print("Computer chose: ");print(random.choice(["Rock", "Paper", "Scissors"]))
   #Determine winner and state what happened to the user
   #Ask the user if they would like to play again.
+  input("Play again? (Y/N):")
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
